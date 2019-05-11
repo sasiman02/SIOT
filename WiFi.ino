d529aa175383de08705727cbc751fd06b0a33161
sasiman02 <gustndlfrl@gmail.com>
2019-05-12 05:16:00 +0900

    complete

--- WiFi.ino
+++ WiFi.ino
@@ -1,19 +1,21 @@
-//*******************************************************************************
-// Project : ESP8266 ESP-01 in Advenced set(Get Weather)
-// Board : Arduino Uno 
-// By : Kit Plus
-//*******************************************************************************/
 #include "WiFiEsp.h"
 #include "SoftwareSerial.h"
+
 SoftwareSerial esp(3, 2); // RX, TX
 char ssid[] = "김현수의 iPhone";            // Your AP SSID
 char pass[] = "999999999";               // Your AR Password
 int status = WL_IDLE_STATUS;        // Status
 unsigned long ulPreTime = 0;
 bool flag_stop;
+int trig = A4;
+int echo = A5;
+int dist;
 
 void setup()
 {
+  pinMode(13,OUTPUT);
+  pinMode(trig,OUTPUT);
+  pinMode(echo,INPUT);
   Serial.begin(9600);
   esp.begin(9600);
   Serial.println("Start get weather forecast");
@@ -30,9 +32,7 @@ void setup()
   {
     status = WiFi.begin(ssid, pass);
   }
-  pinMode(13,OUTPUT);
 }
-String preTime="00000000";
 const String KMA_url = "/wid/queryDFSRSS.jsp?zone=1117053000";
 const char* wSERVER = "www.kma.go.kr";
 #define httpPort 80
@@ -40,7 +40,6 @@ int GetWeatherInformWIFI() {
   WiFiEspClient client;
   int i=0;
   String temp;
-  String nowTime;
   String tmp_str;
   int cnt=0;
   int res=1;
@@ -58,32 +57,40 @@ int GetWeatherInformWIFI() {
     yield();
     String line = client.readStringUntil('\n');
    
-    // search forecast weather string
     i = line.indexOf("</tm>");
     if(i > 0) {
       tmp_str="<tm>";
-      nowTime=line.substring(line.indexOf(tmp_str)+tmp_str.length(),i - 4);
-      Serial.println(nowTime);
+      temp=line.substring(line.indexOf(tmp_str)+tmp_str.length(),i - 4);
+      Serial.print(temp + " ");
     }
-
-//      preTime = nowTime;
-      i= line.indexOf("</wfEn>");
-      if(i>0){
-        tmp_str="<wfEn>";
-        temp = line.substring(line.indexOf(tmp_str)+tmp_str.length(),i);
-        Serial.println(temp);
-        if(!preTime.equals(nowTime) && (temp.equals("Rain") || temp.equals("Snow/Rain")|| temp.equals("Clear"))) {
-            preTime = nowTime;
-            Serial.println("Buzzer On");
-            analogWrite(13,255);
-            delay(100);
-            analogWrite(13,0);
-            Serial.println("Buzzer Off");
-        }
-        
-      res=0;
-      break;
+    i= line.indexOf("</wfEn>");
+    if(i>0){
+      tmp_str="<wfEn>";
+      temp = line.substring(line.indexOf(tmp_str)+tmp_str.length(),i);
+      Serial.println(temp);
+      if((temp.equals("Rain") || temp.equals("Snow/Rain") || temp.equals("Clear"))) {
+          digitalWrite(trig, LOW);
+          delayMicroseconds(2);
+                   
+          digitalWrite(trig,HIGH);
+          delayMicroseconds(10);
+                    
+          digitalWrite(echo, LOW);
+          long duration = pulseIn(echo, HIGH);
+                    
+          dist = duration / 29 / 2;
+          if(dist < 7) {
+            digitalWrite(13,HIGH);
+          }else {
+            digitalWrite(13,LOW);
+          }
+      } else {
+        digitalWrite(13,LOW);
       }
+      
+     res=0;
+     break;
+     }
    }
   if(res == 1){ 
     Serial.println("Wifi weather Fail!");
@@ -96,10 +103,10 @@ int GetWeatherInformWIFI() {
 }
 void loop()
 {
+
   unsigned long ulCurTime = millis();
   // period 1 sec
-  if (ulCurTime - ulPreTime >= 1000)
-  {
+  if (ulCurTime - ulPreTime >= 1000)  {
     ulPreTime = ulCurTime;
     GetWeatherInformWIFI();
   }
